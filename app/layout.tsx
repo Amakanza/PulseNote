085138621dacbc58f50c6a29497f0896a239905b
Amakanza <andilemakanza27@gmail.com>
2025-08-22 18:44:35 +0200

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
