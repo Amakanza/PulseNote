8f24b6ea32ee6a448dc1e894d1cffa64f57068c2
Amakanza <andilemakanza27@gmail.com>
2025-08-22 05:47:17 +0200

    Delete typess directory

--- typess/html-to-docx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "html-to-docx" {
-  // Minimal signature to satisfy TS; widen as needed.
-  const htmlToDocx: (
-    html: string,
-    styles?: any | null,
-    options?: any
-  ) => Promise<ArrayBuffer | Uint8Array | Buffer>;
-  export default htmlToDocx;
-}
